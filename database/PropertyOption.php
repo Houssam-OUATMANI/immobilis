0b0827f4efe0e597f1303c4f7916e5a6b5595d8a
Houssam OUATMMANI <ouatmanihoussam@gmail.com>
2023-10-24 16:48:46 +0200

    fix: retour prematuré

--- database/PropertyOption.php
+++ database/PropertyOption.php
@@ -8,10 +8,12 @@ class PropertyOption
 
 
         public  function store(int $propertyId, array $optionsIds) {
+            $result =false;
             foreach ($optionsIds as $optionId) {
                 $query = "INSERT INTO immobilis.properties_options(properties_id, options_id) VALUES (?, ?)";
                 $stmt = $this->pdo->prepare($query);
-                return  $stmt->execute([$propertyId, $optionId]);
+                $result = $stmt->execute([$propertyId, $optionId]);
             }
+            return $result;
         }
 }
